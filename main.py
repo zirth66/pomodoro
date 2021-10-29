cd2809fe57b75cc008e520f2bc435e3666a6c084
zirth <zirth66@gmail.com>
2021-10-29 18:27:36 +0200

    UI completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from tkinter import *
+from tkinter import font
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +17,32 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+
+#Timer label
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column=1, row=0)
+
+#Clock text
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+#Startbutton
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+#Resetbutton
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+#Checkmark
+checkmark_label = Label(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+checkmark_label.grid(column=1, row=3)
+
+window.mainloop()
